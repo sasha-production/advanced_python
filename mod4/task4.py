b2af364589bdba76e5607a8298ea04eebf267155
sasha <129290522+sasha-production@users.noreply.github.com>
2024-03-16 22:00:29 +0500

    Update task4.py

--- mod4/task4.py
+++ mod4/task4.py
@@ -1,19 +1,28 @@
 from flask import Flask
 import sys
+import shlex
+import subprocess
 
 app = Flask(__name__)
 
 # uptime -p | python3 mod4/task4.py
-UPTIME = sys.stdin.read()
+# UPTIME = sys.stdin.read()
 
 
 @app.route("/uptime", methods=['GET'])
 def uptime() -> str:
-    return f"Current uptime is {UPTIME}"
+    command_str = f"uptime"
+    result = subprocess.run([command_str, '-p'], capture_output=True).stdout.decode('utf8')
+    return f"Current uptime is {result}"
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
 
 
 
